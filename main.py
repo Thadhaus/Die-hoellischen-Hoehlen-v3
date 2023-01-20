f102a03d0caa3a573b959ed913fb56816a16e6ca
Geduldige Birne <106613623+Thadhaus@users.noreply.github.com>
2023-01-20 11:31:07 +0000

    one more translation + autoformat

--- main.py
+++ main.py
@@ -9,54 +9,62 @@ import time
 import sys
 import random
 
+
 # Diese Funktion prüft, ob man einen Schlüssel für den Raum hat.
 # Zuerst einmal fragt sie den Spieler höflich, das reicht zum Testen.
 def check_key():
     key = input("Do you have the key? ").lower()
-    if key in [ "j", "ja", "y", "yes" ]:
+    if key in ["j", "ja", "y", "yes"]:
         return True
     else:
         return False
+
 
 '''
 # Eine Funktion für das Mitnehmen von Zeug, ursprünglich einmal mit fiesen Fallen
 # return: True -> du bist raus, also gestorben, hier nicht genutzt
 # Diese Funktion nimmt noch nichts mit, da dafür noch keine Datenstrukturen vorhanden sind
 '''
+
+
 def take(raum):
     global hungerstatus
     global rauminhalt
     gefunden = False
     ding = input("What do you want to take? ").lower().rstrip()
-    anzahl,zeug = rauminhalt[raum].split(' ',1)
+    anzahl, zeug = rauminhalt[raum].split(' ', 1)
     if ding == zeug:
-        print (f"Each of the {anzahl} {zeug} is too heavy to take with you.")
+        print(f"Each of the {anzahl} {zeug} is too heavy to take with you.")
         gefunden = True
         ''' man sollte etwas essen können '''
         if zeug == 'lichen' and int(anzahl) > 0:
             ''' außer Flechten gibt es nichts essbares in diesen Höhlen '''
-            hungerstatus += random.randint(7,24)
+            hungerstatus += random.randint(7, 24)
             ''' die Flechten werden weniger '''
-            rauminhalt[raum] = "{} {}".format(int(anzahl)-1,zeug)
-    if ( gefunden == False ):
-        print ("I see here no %s" % (ding) )
+            rauminhalt[raum] = "{} {}".format(int(anzahl) - 1, zeug)
+    if (gefunden == False):
+        print("I see here no %s" % (ding))
     return False
+
 
 # Diese Funktion gibt den Hungerlevel aus
 # Andere Effekte können die Variable hungerstatus abfragen
 def check_starvation(hungerlevel):
     if hungerlevel < 1:
-        print ("You die from lack of food ...")
+        print("You die from lack of food ...")
         return True
     elif hungerlevel < 10:
-        print ("You are so weak that you can hardly walk.")
+        print("You are so weak that you can hardly walk.")
     elif hungerlevel < 20:
-        print ("Your stomach growls so loudly that you are afraid of attracting monsters.")
+        print(
+            "Your stomach growls so loudly that you are afraid of attracting monsters."
+        )
     elif hungerlevel < 50:
-        print ("You feel weak. Something to eat would be good now.")
+        print("You feel weak. Something to eat would be good now.")
     else:
-        print ("You are well. You are full and happy.")
+        print("You are well. You are full and happy.")
     return False
+
 
 '''
  Beten hilft, wenn man keinen Ausweg hat. Das entspricht
@@ -65,66 +73,76 @@ def check_starvation(hungerlevel):
  Hier könnte man das Spiel erweitern. Einen Zähler einbauen,
  und das Beten begrenzen. Oder Gegenrechnen mit erreichten Zielen...
  '''
+
+
 def pray():
     print("A strange trembling passes through the room.")
     verbindungen_erzeugen()
+
 
 # Zur Zeit ist nur das Biom in 'look_around' und der Rauminhalt ist
 # lediglich generiert
 def zeige_rauminhalt():
     inhalt = rauminhalt[current_room]
-    print ( inhalt )
+    print(inhalt)
+
 
 # Man kann das Spiel jederzeit verlassen.
 def quit():
-    print ("There is a bang. Smoke rises. You faint.")
-    for i in range(3):      # default: 23 Sekunden, Debug: 3 Sekunden
+    print("There is a bang. Smoke rises. You faint.")
+    for i in range(3):  # default: 23 Sekunden, Debug: 3 Sekunden
         time.sleep(1)
-        print (".",end="")
+        print(".", end="")
         sys.stdout.flush()  # damit die Punkte einzeln erscheinen
-    print ("\n\nYou wake up in a meadow of flowers and wonder:")
-    print ("Was I really in those strange caves")
-    print ("or was it all just a strange dream?")
+    print("\n\nYou wake up in a meadow of flowers and wonder:")
+    print("Was I really in those strange caves")
+    print("or was it all just a strange dream?")
+
 
 # Die kurze Anleitung gibt bisher einfach nur die Kommandowörter aus
 def usage():
-    print ("\tThe following command words are known to the system:\n\n\t",end="")
+    print("\tThe following command words are known to the system:\n\n\t",
+          end="")
     for wort in allowed_commands:
         # Mache 'tp' (Teleport) zu einem geheimen Kommando
         if wort == "tp" or wort == 'teleport':
             pass
         else:
-            print ("{} ".format(wort),end="")
-    print ("\n\n\tViel Spaß\n")
+            print("{} ".format(wort), end="")
+    print("\n\n\tHave fun!\n")
+
 
 def generate_graphviz_file():
-  print ("Well, there are no maps to show...")
-  import os
-  import glob
-  import generiere_karte
-  os.system("mkdir -p Hoelle-Karten")
-  result = glob.glob(r"Hoelle-Karten/*.py")
-  if len(result) >  0:
-    print(f"Welche Datei soll in einen Graphen umgewandelt werden?")
-    for i in range(len(result)):
-      print(f"{i+1} : {result[i]}")
-    num = int(input("Gib mir die Nummer deiner Wunschdatei: "))
-    wunschdatei = result[num-1]
-    generiere_karte.hauptprogramm(wunschdatei)
-    antwort = input(f"Die Datei {wunschdatei} jetzt löschen?").lower()
-    if antwort in ['y','j','yes','ja']:
-      os.system(f"rm -f {wunschdatei}")
-    
+    print("Well, there are no maps to show...")
+    import os
+    import glob
+    import generiere_karte
+    os.system("mkdir -p Hoelle-Karten")
+    result = glob.glob(r"Hoelle-Karten/*.py")
+    if len(result) > 0:
+        print(f"Welche Datei soll in einen Graphen umgewandelt werden?")
+        for i in range(len(result)):
+            print(f"{i+1} : {result[i]}")
+        num = int(input("Gib mir die Nummer deiner Wunschdatei: "))
+        wunschdatei = result[num - 1]
+        generiere_karte.hauptprogramm(wunschdatei)
+        antwort = input(f"Die Datei {wunschdatei} jetzt löschen?").lower()
+        if antwort in ['y', 'j', 'yes', 'ja']:
+            os.system(f"rm -f {wunschdatei}")
+
 
 # Where can I go from each direction?
 # j -> down ; k -> up ; q -> quit
-allowed_commands = ["q", "w", "a", "s", "d", "j", "k", "look", "take", "pray", "map", "help"]
+allowed_commands = [
+    "q", "w", "a", "s", "d", "j", "k", "look", "take", "pray", "map", "help"
+]
 
 # Zufällig generierte Höhlen in fast beliebiger Zahl und Menge
 # Begrenzend ist die Zahl der nutzbaren Zeichen, aber man darf
 # UTF-8-Zeichen verwenden, wenn man möchte
-raumliste = list('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789') # 62 Räume billig erzeugt
-
+raumliste = list(
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+)  # 62 Räume billig erzeugt
 '''
 Hier werden die Verbindungen der Räume in der Raumliste
 zufällig verbunden, mit einer gewissen Wahrscheinlichkeit,
@@ -132,6 +150,8 @@ damit es nicht zuviele Verbindungen gibt.
 Notfalls - wenn es keinen Ausweg gibt - kann der Spieler
 beten 'pray', um ein neues Set Verbindungen zu erzeugen
 '''
+
+
 # Was mir gut gefallen hat, ist die Idee, die speziellen Räume
 # aus der Raumliste zu nehmen, bevor man wilde zufällige Dinge
 # generiert, und anschließend die speziellen Räume wieder gezielt
@@ -142,6 +162,7 @@ def generiere_ziel():
     else:
         return None
 
+
 north = {}
 south = {}
 east = {}
@@ -149,21 +170,30 @@ west = {}
 upstairs = {}
 downstairs = {}
 
+
 def verbindungen_erzeugen():
     for raum in raumliste:
         for richtung in east, west, north, south, upstairs, downstairs:
             richtung[raum] = generiere_ziel()
 
-    
+
 verbindungen_erzeugen()
 
 # Put the directions into a compass dictionary
-compass = { "w": north, "a": west, "s": south, "d": east, "k": upstairs, "j": downstairs }
+compass = {
+    "w": north,
+    "a": west,
+    "s": south,
+    "d": east,
+    "k": upstairs,
+    "j": downstairs
+}
 
 import metagenerator as generator
+
 raumbeschreibung, rauminhalt, raumbiome = generator.hauptprogramm(raumliste)
 description = raumbeschreibung
-look_around = raumbiome # billiger 'hack', um überhaupt etwas in 'look_around' zu haben
+look_around = raumbiome  # billiger 'hack', um überhaupt etwas in 'look_around' zu haben
 #rauminhalt = rauminhalt
 
 # Put in the number of the room you want them to start in
@@ -185,11 +215,11 @@ while final_room == current_room:
 command = ""
 hungerstatus = 100
 raumwechsel_erfolgt = True
-print ("\n\t*** Welcome to the hellish caves ***\n")
+print("\n\t*** Welcome to the hellish caves ***\n")
 usage()
-while( current_room is not None ):
+while (current_room is not None):
     if raumwechsel_erfolgt:
-        print (f"\nYou are here: {description[current_room]}. ",end='')
+        print(f"\nYou are here: {description[current_room]}. ", end='')
     else:
         raumwechsel_erfolgt = True
     hungerstatus = hungerstatus - 1
@@ -203,13 +233,13 @@ while( current_room is not None ):
         command = input("Which direction do you want to go? ").lower()
     # Man kann verhungern ...
     if command == "q":
-        quit()      #   das Spiel freiwillig aufgeben :-/
+        quit()  #   das Spiel freiwillig aufgeben :-/
         current_room = None
     elif command == "look":
         zeige_rauminhalt()
     elif command == "take":
-        if ( take(current_room) ):
-            current_room = None     # you managed to die :-)
+        if (take(current_room)):
+            current_room = None  # you managed to die :-)
     # Wenn sonst nichts geht, kann man beten und bekommt neue Verbindungen.
     elif command == "pray":
         pray()
@@ -230,20 +260,23 @@ while( current_room is not None ):
                 current_room = previous_room
         # See if they are in the final room
         if current_room == final_room:
-            result = input ("Do you really want to leave the caves? [yes/No] ").lower()
+            result = input(
+                "Do you really want to leave the caves? [yes/No] ").lower()
             if result == "yes" or result == "y":
-                current_room = None # Ends the game loop
+                current_room = None  # Ends the game loop
                 print("""
 After all the hours spent in these interesting caves full of valuable treasures you decide to go out into the daylight.
 """)
-                print("You step through the waterfall and walk out.\nJust like that, and now it's all over.")
+                print(
+                    "You step through the waterfall and walk out.\nJust like that, and now it's all over."
+                )
             else:
                 continue
         # Erst und nur nach dem Wechsel in die nächste Höhle wird das Labyrinth neu vernetzt.
         # Das gibt dem Spieler die Möglichkeit, verschiedene Richtungen zu probieren.
-        verbindungen_erzeugen() # In der Hölle bleibt kein Weg, wo er eben noch war.
+        verbindungen_erzeugen(
+        )  # In der Hölle bleibt kein Weg, wo er eben noch war.
 
     else:
-        print ("Boom. You bounce off. It doesn't go that way. ",end="")
+        print("Boom. You bounce off. It doesn't go that way. ", end="")
         raumwechsel_erfolgt = False
-
